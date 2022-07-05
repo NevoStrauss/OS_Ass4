4343406e146fcb5178280992813bee36c08b7393
Eran Krauss <74011287+EranKrauss@users.noreply.github.com>
2022-07-05 14:47:03 +0300

    fixing ln

--- user/ln.c
+++ user/ln.c
@@ -5,11 +5,17 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3){
-    fprintf(2, "Usage: ln old new\n");
+  char* s = "-s";
+  if((argc != 3 || (argc == 3 && strcmp(argv[1], s) == 0)) && (argc != 4 || (argc == 4 && strcmp(argv[1], s) != 0))){
     exit(1);
   }
-  if(link(argv[1], argv[2]) < 0)
-    fprintf(2, "link %s %s: failed\n", argv[1], argv[2]);
+  if(argc == 3){
+    if(link(argv[1], argv[2]) < 0)
+      fprintf(2, "link failed\n");
+  } else{
+    if(symlink(argv[2], argv[3]) < 0){
+      fprintf(2, "symbolic link failed\n");
+    }
+  }
   exit(0);
 }
